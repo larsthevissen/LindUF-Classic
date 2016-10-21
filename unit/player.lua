1f3305b19d3d0d328bbac3f71be9e2fb19394e77
Lars Thevißen <larthev@googlemail.com>
2016-10-21 17:37:23 +0200

    raidtarget

--- unit/player.lua
+++ unit/player.lua
@@ -13,7 +13,7 @@ PlayerFrame:SetPoint("CENTER", player, "CENTER", 0, 0)
 
 player.menu = function(self, unit, button, actionType)
   -- ToggleDropDownMenu(1, nil, PlayerFrameDropDown, self, 0, 0)
-  ToggleDropDownMenu(1, nil, LindUF.DropDown, self, 0, 0)
+  ToggleDropDownMenu(1, nil, LindUF.DropDown, "cursor", 0, 0)
 end
 
 
@@ -36,43 +36,42 @@ player.Name:SetPoint("LEFT", player.HealthBar, "Left", 2, 0)
 
 
 -- DROPDOWN START --
-local favoriteNumber = 42 -- A user-configurable setting
-
--- Create the dropdown, and configure its appearance
 LindUF.DropDown = CreateFrame("FRAME", "WPDemoDropDown", UIParent, "UIDropDownMenuTemplate")
 LindUF.DropDown:SetPoint("CENTER")
 UIDropDownMenu_SetWidth(LindUF.DropDown, 200)
---UIDropDownMenu_SetText(LindUF.DropDown, "Favorite number: " .. favoriteNumber)
 
--- Create and bind the initialization function to the dropdown menu
 UIDropDownMenu_Initialize(LindUF.DropDown, function(self, level, menuList)
  local info = UIDropDownMenu_CreateInfo()
- if (level or 1) == 1 then
-  -- Display the 0-9, 10-19, ... groups
-  for i=0,4 do
-   info.text, info.checked = i*10 .. " - " .. (i*10+9), favoriteNumber >= i*10 and favoriteNumber <= (i*10+9)
-   info.menuList, info.hasArrow = i, true
+ if level == 1 then
+   info.text = "RaidTargetIcon"
+   info.hasArrow = true
+   info.notCheckable = true
    UIDropDownMenu_AddButton(info)
-  end
-
- else
-  -- Display a nested group of 10 favorite number options
-  info.func = self.SetValue
-  for i=menuList*10, menuList*10+9 do
-   info.text, info.arg1, info.checked = i, i, i == favoriteNumber
-   UIDropDownMenu_AddButton(info, level)
-  end
  end
+
+ if level == 2 then
+   local index = GetRaidTargetIndex("player")
+
+   for i = 1, 8 do
+     if index == i then
+       info.checked = true
+     else
+       info.checked = false
+     end
+
+     info.text = "_"
+     info.func = self.SetMarker
+     info.icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_"..i
+     info.arg1 = i
+     UIDropDownMenu_AddButton(info, 2)
+   end
+ end
+
 end,
 "MENU")
 
--- Implement the function to change the favoriteNumber
-function LindUF.DropDown:SetValue(newValue)
- favoriteNumber = newValue
- -- Update the text; if we merely wanted it to display newValue, we would not need to do this
- UIDropDownMenu_SetText(LindUF.DropDown, "Favorite number: " .. favoriteNumber)
- -- Because this is called from a sub-menu, only that menu level is closed by default.
- -- Close the entire menu with this next call
+function LindUF.DropDown:SetMarker(value)
+SetRaidTarget("player",value)
  CloseDropDownMenus()
 end
 -- DROPDOWN END --
